bedecb29e807a937ae799dfd09f1327d0aebd0e5
John <Xohnjoseph@gmail.com>
2017-09-26 00:18:32 +0700

    toggle visibility

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
 "use strict";
+
+var visibility = false;
+var onButtonToggle = function onButtonToggle() {
+  visibility = !visibility;
+  render();
+};
+var render = function render() {
+  var visbility = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      "Visibity Toggle"
+    ),
+    React.createElement(
+      "button",
+      { onClick: onButtonToggle },
+      visibility ? "hide details" : "show details"
+    ),
+    visibility && React.createElement(
+      "p",
+      null,
+      "hey, There are some details now you can see!"
+    )
+  );
+  ReactDOM.render(visbility, document.getElementById("app"));
+};
+
+render();
---
+++ src/app.js
@@ -0,0 +1,17 @@
+let visibility=false;
+const onButtonToggle=()=>{
+  visibility= !visibility;
+  render();
+  
+}
+const render=()=>{const visbility=(
+  <div>
+    <h1>Visibity Toggle</h1>
+    <button onClick={onButtonToggle}>{visibility? "hide details":"show details"}</button>
+    {visibility && <p>hey, There are some details now you can see!</p>}
+  </div>
+)
+ReactDOM.render(visbility,document.getElementById("app"))
+}
+
+render();
